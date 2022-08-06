decd6318c1197b4a135211e3ab339caa6844dfd7
Bella Huang <bella.huang@yourenext.ca>
2022-08-06 16:41:51 -0400

    implement merge sort

--- src/SortingVisualizer/SortingVisualizer.jsx
+++ src/SortingVisualizer/SortingVisualizer.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import * as sortingAlgorithms from '../sortingAlgorithms/sortingAlgorithms'
 import "./SortingVisualizer.css";
 
 export default class SortingVisualizer extends React.Component {
@@ -24,18 +25,32 @@ export default class SortingVisualizer extends React.Component {
     this.setState({ array });
   }
 
+  mergeSort() {
+    const javaScriptSortedArray = this.state.array.slice().sort((a, b) => a - b);
+    const sortedArray = sortingAlgorithms.mergeSort(this.state.array);
+    console.log(arraysAreEqual(javaScriptSortedArray, sortedArray));
+  }
+  quickSort() {}
+  heapSort() {}
+  bubbleSort() {}
+
+
   render() {
     const { array } = this.state;
     return (
       <div className="array-container">
         {array.map((value, index) => (
-          <div 
-            className="array-bar" 
+          <div
+            className="array-bar"
             key={index}
-            style={{height: `${value}px`}}>
-          </div>
+            style={{ height: `${value}px` }}
+          ></div>
         ))}
         <button onClick={() => this.resetArray()}>Generate New Array</button>
+        <button onClick={() => this.mergeSort()}>Merge Sort</button>
+        <button onClick={() => this.quickSort()}>Quick Sort</button>
+        <button onClick={() => this.heapSort()}>Heap Sort</button>
+        <button onClick={() => this.bubbleSort()}>Bubble Sort</button>
       </div>
     );
   }
@@ -43,4 +58,16 @@ export default class SortingVisualizer extends React.Component {
 
 function randomNumber(min, max) {
   return Math.floor(Math.random() * (max - min)) + min;
+}
+
+function arraysAreEqual(arrayOne, arrayTwo) {
+  if (arrayOne.length !== arrayTwo.length) {
+    return false;
+  }
+  for (let i = 0; i < arrayOne.length; i++) {
+    if (arrayOne[i] !== arrayTwo[i]) {
+      return false;
+    }
+  }
+  return true;
 }
